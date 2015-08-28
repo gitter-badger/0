d73f529112259c86d578ec4cd290cacd165e5f2d
Christoph Dorn <christoph@christophdorn.com>
2015-08-27 22:30:10 -0700

    wip

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+if [ -z "$npm_config_argv" ]; then
+	echo "ERROR: Must run with 'npm install'!"
+	exit 1
+fi
 if [ -z "$HOME" ]; then
 	echo "ERROR: 'HOME' environment variable is not set!"
 	exit 1
 fi
+WORKSPACE_DIR="$PWD"
 
-echo "dir"
 
-pwd
+pushd "$WORKSPACE_DIR" > /dev/null
+	if [ -f ".gitmodules" ]; then
+		if [ ! -f ".gitmodules.initialized" ]; then
+			git submodule update --init --recursive --rebase
+			touch ".gitmodules.initialized"
+		fi
+	fi
 
-#lib/bash.origin/bash.origin BO install; 
+	lib/bash.origin/bash.origin BO install
 
-exit 1
+popd > /dev/null
+
 
 # Source https://github.com/bash-origin/bash.origin
 . "$HOME/.bash.origin"
@@ -20,19 +31,6 @@ function init {
 	local __BO_DIR__="$___TMP___"
 
 	BO_log "$VERBOSE" "HEADER" "Installing 0 ..."
-
-	pushd "$__BO_DIR__/.." > /dev/null
-		if [ -f ".gitmodules" ]; then
-			if [ ! -f ".gitmodules.initialized" ]; then
-				BO_log "$VERBOSE" "Init submodules ..."
-				git submodule update --init --recursive --rebase
-				BO_log "$VERBOSE" "... submodules init done"
-				touch ".gitmodules.initialized"
-			else
-				BO_log "$VERBOSE" "Skip init submodules. Already initialized."
-			fi
-		fi
-	popd > /dev/null
 
 #	BO_ensure_nvm
 #	nvm install 0.12
