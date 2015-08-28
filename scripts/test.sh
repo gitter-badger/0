322fa450ea0a215613310c8df94850ace1c40326
Christoph Dorn <christoph@christophdorn.com>
2015-08-28 00:03:46 -0700

    travis ci

---
+++ scripts/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ -z "$HOME" ]; then
+	echo "ERROR: 'HOME' environment variable is not set!"
+	exit 1
+fi
+# Source https://github.com/bash-origin/bash.origin
+. "$HOME/.bash.origin"
+function init {
+	eval BO_SELF_BASH_SOURCE="$BO_READ_SELF_BASH_SOURCE"
+	BO_deriveSelfDir ___TMP___ "$BO_SELF_BASH_SOURCE"
+	local __BO_DIR__="$___TMP___"
+
+
+	function Test {
+		BO_log "$VERBOSE" "HEADER" "Testing ..."
+
+echo "Running ..."
+
+		BO_log "$VERBOSE" "FOOTER"
+	}
+
+
+	Test $@
+
+}
+init $@
