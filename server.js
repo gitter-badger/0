02d66bdddde8c665f9bf0c70e01b492fff84260f
Christoph Dorn <christoph@christophdorn.com>
2015-08-27 21:46:42 -0700

    fix

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var port = process.env.PORT;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World from zerosystem.io\n');
-}).listen(port, '127.0.0.1');
+}).listen(port, '0.0.0.0');
 
 console.log('Server running at http://127.0.0.1:' + port + '/');
